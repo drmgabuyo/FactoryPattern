6783046865ae6054c67d5efec2b7f9acefd902ee
Dyna Rose Gabuyo <147422759+drmgabuyo@users.noreply.github.com>
2023-11-17 02:40:34 +0800

    Update Customer.java

--- src/factory/Customer.java
+++ src/factory/Customer.java
@@ -2,46 +2,46 @@ package factory;
 
 import java.util.Scanner;
 
-public class Clinic {
+public class Customer {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Choose a pet");
-        System.out.println("1. Dog");
-        System.out.println("2. Cat");
+        System.out.println("Choose a flower");
+        System.out.println("1. Rose");
+        System.out.println("2. Tulip");
         System.out.println("3. Exit");
         System.out.print("Enter number: ");
         Integer choice = input.nextInt();
 
-        PetRecord petRecord = new PetRecord();
-        Pet pet;
+        FlowerRecord flowerRecord = new FlowerRecord();
+        Flower flower;
 
         switch (choice) {
             case 1:
-                pet = new Dog();
-                petRecord.setPetName("Bantay");
-                petRecord.setPetId("P001");
-                petRecord.setPet(pet);
-                ((Dog) pet).setBreed("German shepherd");
+                flower = new Rose();
+                flowerRecord.setFlowerName("Rose");
+                flowerRecord.setFlowerId("RS");
+                flowerRecord.setFlower(flower);
+                ((Rose) flower).setColor("Red");
                 break;
 
             case 2:
-                pet = new Cat();
-                petRecord.setPetName("Muning");
-                petRecord.setPetId("P002");
-                petRecord.setPet(pet);
-                ((Cat) pet).setNoOfLives(9);
+                flower = new Tulip();
+                flowerRecord.setFlowerName("Tulip");
+                flowerRecord.setFlowerId("TP");
+                flowerRecord.setFlower(flower);
+                ((Tulip) pet).setNoOfPetals(6);
                 break;
             case 3:
                 System.exit(0);
 
         }
 
-        System.out.println("Pet id is " + petRecord.getPetId());
-        System.out.println("Pet name is " + petRecord.getPetName());
-        System.out.println("Pet kind: " + petRecord.getPet().getClass().getSimpleName());
-        System.out.println("Communication sound: " + petRecord.getPet().makeSound());
-        System.out.println("Play mode: " + petRecord.getPet().play());
+        System.out.println("Flower id is " + flowerRecord.getFlowerId());
+        System.out.println("Flower name is " + flowerRecord.getFlowerName());
+        System.out.println("Flower species: " + flowerRecord.getFlower().getClass().getSimpleName());
+        System.out.println("Is it blooming? " + flowerRecord.getFlower().bloom());
+        System.out.println("Does it smells good? " + flowerRecord.getFlower().fragrance());
 
 
     }
